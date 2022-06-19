a8f01195493d5d87c40452242ef47b0f69068eac
danielsrw <danielsrwanda@gmail.com>
2022-06-19 17:51:51 +0200

    fetch data

--- src/components/HorizontalScrollbar.jsx
+++ src/components/HorizontalScrollbar.jsx
@@ -25,10 +25,14 @@ const RightArrow = () => {
     );
 };
 
-const HorizontalScrollbar = () => (
-    <>
-        I'm HorizontalScrollbar
-    </>
+const HorizontalScrollbar = ({ data, bodyParts, setBodyPart, bodyPart }) => (
+    <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
+        {data.map((item) => (
+            <Box key={item.id || item} itemId={item.id || item} title={item.id || item} m="0 40px">
+                {bodyParts ? <BodyPart item={item} setBodyPart={setBodyPart} bodyPart={bodyPart} /> : <ExerciseCard exercise={item} /> }
+            </Box>
+        ))}
+    </ScrollMenu>
 );
 
 export default HorizontalScrollbar;
