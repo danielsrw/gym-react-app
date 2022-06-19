d368b2b56d3647e5eb311863caa71750eb9c3380
danielsrw <danielsrwanda@gmail.com>
2022-06-19 18:40:18 +0200

    final release

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Box, Stack, Typography } from '@mui/material';
 import Logo from '../assets/images/Logo-1.png';
+import { Box, Stack, Typography } from '@mui/material';
 
 const Footer = () => {
     return (
@@ -9,7 +9,7 @@ const Footer = () => {
                 <Stack gap="40px" sx={{ alignItems: 'center' }} flexWrap="wrap" px="40px" pt="24px">
                     <img src={Logo} alt="logo" style={{ width: '200px', height: '41px' }} />
                 </Stack>
-                <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" pb="40px">Made with ❤️ by JavaScript Mastery</Typography>
+                <Typography variant="h5" sx={{ fontSize: { lg: '18px', xs: '14px' } }} mt="41px" textAlign="center" pb="40px">Have fun ❤️</Typography>
             </Box>
         </>
     )
--- src/components/HorizontalScrollbar.jsx
+++ src/components/HorizontalScrollbar.jsx
@@ -28,7 +28,7 @@ const RightArrow = () => {
 const HorizontalScrollbar = ({ data, bodyParts, setBodyPart, bodyPart }) => (
     <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
         {data.map((item) => (
-            <Box key={item.id || item} itemId={item.id || item} title={item.id || item} m="0 40px">
+            <Box key={item.id || item} itemId={item.id || item} title={item.id || item} m="0 40px" >
                 {bodyParts ? <BodyPart item={item} setBodyPart={setBodyPart} bodyPart={bodyPart} /> : <ExerciseCard exercise={item} /> }
             </Box>
         ))}
