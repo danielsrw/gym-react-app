e37d94e69fd8f2636ee531bde1bffadbef6f0557
danielsrw <danielsrwanda@gmail.com>
2022-06-19 17:27:18 +0200

    hero banner

--- src/components/HorizontalScrollbar.jsx
+++ src/components/HorizontalScrollbar.jsx
@@ -1,4 +1,29 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { ExerciseCard, BodyPart } from './';
+import { Box, Typography } from '@mui/material';
+import LeftArrowIcon from '../assets/icons/left-arrow.png';
+import RightArrowIcon from '../assets/icons/right-arrow.png';
+import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
+
+const LeftArrow = () => {
+    const { scrollPrev } = useContext(VisibilityContext);
+
+    return (
+        <Typography onClick={() => scrollPrev()} className="right-arrow">
+            <img src={LeftArrowIcon} alt="right-arrow" />
+        </Typography>
+    );
+};
+
+const RightArrow = () => {
+    const { scrollNext } = useContext(VisibilityContext);
+
+    return (
+        <Typography onClick={() => scrollNext()} className="left-arrow">
+            <img src={RightArrowIcon} alt="right-arrow" />
+        </Typography>
+    );
+};
 
 const HorizontalScrollbar = () => (
     <>
